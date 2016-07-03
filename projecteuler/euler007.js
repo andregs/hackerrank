f4bc6efe694a0f9eeebd18f57e4099761ee095e7
André Gomes <andregs@users.noreply.github.com>
2016-07-03 19:15:35 -0300

    Project Euler 7: 10001st prime

---
+++ projecteuler/euler007.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler007
+// tip: create an efficient prime generator, like the Sieve of Eratosthenes
+
+function processLine(n) {
+    while (primes.length < n)
+        primes.push(gen.next().value);
+
+    process.stdout.write(primes[n - 1] + "\n");
+}
+
+let gen = primegen();
+let primes = [];
+
+function* primegen() {
+    function* multgen(increment) {
+        let multiple = increment;
+        for (;;) yield multiple += increment;
+    }
+
+    let skip = new Map();
+    for (let i = 2 ;; i++) {
+        if (skip.has(i)) {
+            let gen = skip.get(i);
+            skip.delete(i);
+            let multiple = gen.next().value;
+            while (skip.has(multiple))
+                multiple = gen.next().value;
+            skip.set(multiple, gen);
+        } else {
+            yield i;
+            let gen = multgen(i);
+            skip.set(gen.next().value, gen);
+        }
+    }
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let _input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   _input.split("\n").slice(1).map(Number).forEach(processLine);
+});
---
+++ projecteuler/euler007.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let output = `
+4
+3
+6
+1
+10000
+`;
+
+process.stdout.write(output.trim());
