bbdf01b199eee0d5b0ece108bd6f69497dbfe6cf
André Gomes <andregs@users.noreply.github.com>
2016-07-02 23:13:43 -0300

    Project Euler 5: Smallest multiple

---
+++ projecteuler/euler005.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler005
+// tip: break each number into primes - look at this example for input N=6
+// 6 5 4 3 2 / 2
+// 3 5 2 3 1 / 2
+// 3 5 1 3 1 / 3
+// 1 5 1 1 1 / 5
+// 1 1 1 1 1 => 60 (coz 2*2*3*5)
+
+function processLine(n) {
+    let range = [...Array(n + 1).keys()].slice(2); // [2,3,4, ... n]
+    let answer = range.reduce(smallestMultiple, [1]).reduce((a,b) => a * b);
+    process.stdout.write(answer + "\n");
+}
+
+function smallestMultiple(factors, current) {
+    for (let factor of factors)
+        if (current % factor === 0)
+            current /= factor;
+
+    if (current > 1)
+        for (let prime of primes)
+            if (current % prime === 0)
+                factors.push(prime);
+
+    return factors;
+}
+
+let gen = primegen();
+let primes = [];
+for (let p = gen.next().value; p < 40; p = gen.next().value) {
+    primes.push(p);
+}
+
+function* primegen() {
+    function* multgen(increment) {
+        let multiple = increment;
+        for (;;) yield multiple += increment;
+    }
+
+    let skip = new Map();
+    for (let i = 2 ;; i++) {
+        if (skip.has(i)) {
+            let gen = skip.get(i);
+            skip.delete(i);
+            let multiple = gen.next().value;
+            while (skip.has(multiple))
+                multiple = gen.next().value;
+            skip.set(multiple, gen);
+        } else {
+            yield i;
+            let gen = multgen(i);
+            skip.set(gen.next().value, gen);
+        }
+    }
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let _input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   _input.split("\n").slice(1).map(Number).forEach(processLine);
+});
---
+++ projecteuler/euler005.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let output = `
+4
+3
+10
+1
+40
+`;
+
+process.stdout.write(output.trim());
