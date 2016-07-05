124517e88f467122dbd0a4f2d6d4b31a8e5486b2
André Gomes <andregs@users.noreply.github.com>
2016-07-05 20:58:02 -0300

    Project Euler 10 - Summation of primes

---
+++ projecteuler/euler010.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler010
+// tip: create an efficient prime generator, like the Sieve of Eratosthenes
+
+function processLine(n) {
+    while (sum[n] === undefined) {
+        const prime = gen.next().value;
+        let lastKey = sum.length - 1;
+        const lastValue = sum[lastKey];
+        while (++lastKey < prime) sum[lastKey] = lastValue;
+        sum[prime] = lastValue + prime;
+    }
+    process.stdout.write(sum[n] + "\n");
+}
+
+let gen = primegen();
+let sum = [0];
+
+function* primegen() {
+    function* multgen(increment) {
+        let multiple = increment;
+        for (;;) yield multiple += increment;
+    }
+
+    let skip = new Map();
+    for (let i = 2 ;; i++) {
+        if (skip.has(i)) {
+            let gen = skip.get(i);
+            skip.delete(i);
+            let multiple = gen.next().value;
+            while (skip.has(multiple))
+                multiple = gen.next().value;
+            skip.set(multiple, gen);
+        } else {
+            yield i;
+            let gen = multgen(i);
+            skip.set(gen.next().value, gen);
+        }
+    }
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let _input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   _input.split("\n").slice(1).map(Number).forEach(processLine);
+});
---
+++ projecteuler/euler010.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let output = `
+4
+5
+10
+1
+1000000
+`;
+
+process.stdout.write(output.trim());
