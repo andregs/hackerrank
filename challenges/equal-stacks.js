57dffeeaf52028db2309deb95c7550b848b74675
André Gomes <andregs@users.noreply.github.com>
2016-07-07 06:02:19 -0300

    Implementation - Equal Stacks

---
+++ challenges/equal-stacks.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// https://www.hackerrank.com/challenges/equal-stacks
+// tip: pop one from the heighest stack untill they have same height
+
+function main() {
+    const stacks = input.split('\n').slice(1)
+        .map(line => line.split(' ').reverse().map(Number));
+
+    const heights = [];
+    stacks.forEach(stack => heights.push(stack.reduce((a, b) => a + b)));
+
+    for (let i = heighest(stacks, heights); i !== -1; i = heighest(stacks, heights)) {
+        heights[i] -= stacks[i].pop();
+    }
+
+    process.stdout.write('' + height(stacks[0]));
+}
+
+function heighest(stacks, heights) {
+    if (heights[0] === heights[1] && heights[0] === heights[2]) return -1;
+    if (heights[0] >= heights[1] && heights[0] >= heights[2]) return 0;
+    if (heights[1] >= heights[0] && heights[1] >= heights[2]) return 1;
+    return 2;
+}
+
+function height(stack) {
+    return stack.length && stack.reduce((a, b) => a + b);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+let input = "";
+
+process.stdin.on('data', function (data) {
+    input += data;
+});
+
+process.stdin.on('end', main);
