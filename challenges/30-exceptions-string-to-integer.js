77fb2a79d51d9720d99094726ac698ce674cf9cc
André Gomes <andregs@users.noreply.github.com>
2016-07-10 10:04:44 -0300

    Day 16 - Exceptions - String to Integer 30 Days of Code

---
+++ challenges/30-exceptions-string-to-integer.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input = "";
+
+process.stdin.on('data', function (data) {
+    input += data;
+});
+
+process.stdin.on('end', main);
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    try {
+        var answer = isNaN(+ input) ? null.throw : + input;
+    } catch (e) {
+        answer = 'Bad String';
+    }
+    process.stdout.write(answer.toString());
+}
+
+// DO NOT submit this test case because the solution MUST NOT contain
+// loops or conditional statements
+
+if (process.argv[2] === 'test') {
+    process.stdin.pause();
+    input = '7x768';
+    process.stdout.write(`Input:\n${input}\n\nOutput:\n`);
+    main();
+}
