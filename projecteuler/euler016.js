81c8f0faa9e186023175733031208cdfdbe2aa07
André Gomes <andregs@users.noreply.github.com>
2016-07-11 21:43:21 -0300

    Project Euler 16 - Power digit sum

---
+++ projecteuler/euler016.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler016
+// tip: this is trivial if you use bignumber.js with POW_PRECISION = 0
+
+const BigNumber = require('bignumber.js');
+BigNumber.config({ POW_PRECISION: 0 });
+
+function main() {
+    input.split('\n').slice(1).map(Number).forEach(processLine);
+}
+
+function processLine(n) {
+    process.stdout.write(
+        new BigNumber(2).pow(n, 0)
+            .toFixed().split('').map(Number)
+            .reduce((a, b) => a + b) + '\n'
+    );
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+let input = "";
+process.stdin.on('data', function (data) {
+    input += data;
+});
+
+process.stdin.on('end', main);
+
+if (process.argv[2] === 'test') {
+    process.stdin.pause();
+    input = `
+    5
+    0
+    1
+    2
+    3
+    ${1e4}
+    `.replace(/^\s+/mg, "").trim();
+    process.stderr.write(`Input:\n${input}\n\nOutput:\n`);
+    main();
+}
