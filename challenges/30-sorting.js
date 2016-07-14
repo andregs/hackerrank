b91cf8758e8d7b08e2d04077958f334b3a8c4a88
André Gomes <andregs@users.noreply.github.com>
2016-07-14 10:34:32 -0300

    Day 20 - Sorting 30 Days of Code

---
+++ challenges/30-sorting.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// https://www.hackerrank.com/challenges/30-sorting
+
+function main() {
+    input = input.split('\n').slice(1)[0].split(' ').map(Number);
+    let totalSwaps = 0;
+    for (let i = 0; i < input.length; i++) {
+        let swaps = 0;
+        for (let j = 0; j < input.length - 1; j++) {
+            if (input[j] > input[j + 1]) {
+                [input[j], input[j + 1]] = [input[j + 1], input[j]];
+                swaps++;
+                totalSwaps++;
+            }
+        }
+        if (swaps === 0) break;
+    }
+
+    process.stdout.write(`Array is sorted in ${totalSwaps} swaps.\n`);
+    process.stdout.write(`First Element: ${input[0]}\n`);
+    process.stdout.write(`Last Element: ${input[input.length - 1]}`);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let input = "";
+process.stdin.on("data", function (data) {
+    input += data;
+});
+
+process.stdin.on("end", main);
+
+if (process.argv[2] === 'test') {
+    process.stdin.pause();
+    input = `
+    3
+    3 2 1
+    `.replace(/^\s+/mg, "").trim();
+    process.stdout.write(`Input:\n${input}\n\nOutput:\n`);
+    main();
+}
