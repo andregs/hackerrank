325216c717a0de94d86ef17ddcd2acb6faa5a2cf
Elastic Cloud User <Elastic Cloud User>
2016-11-24 17:56:15 -0200

    Day 27 - Testing

---
+++ tutorials/30-days-of-code/30-testing.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// https://www.hackerrank.com/challenges/30-testing
+
+console.log('5');
+console.log('3 1');
+console.log('-1 0 1');
+console.log('4 2');
+console.log('-1000 0 1000 5');
+console.log('5 3');
+console.log('-3 -2 -1 0 1');
+console.log('6 6');
+console.log('0 20 0 -7 -9 13');
+console.log('7 1');
+console.log('-7 0 5 5 5 5 5');
